9e09ef71c298a68395c57abc098a424af119b655
Devon Aleshire <devon.aleshire@gmail.com>
2020-10-20 15:31:52 -0700

    Add project files.

---
+++ WebPushDemo/Data/WebPushDemoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceWorkerDemo.Models;
+
+namespace ServiceWorkerDemo.Data
+{
+    public class WebPushDemoContext : DbContext
+    {
+        public WebPushDemoContext(DbContextOptions<WebPushDemoContext> options) : base(options) { }
+        public DbSet<Devices> Devices { get; set; }
+    }
+}
---
+++ WebPushDemo/wwwroot/sw-registration.js
@@ -0,0 +1,4 @@
+﻿if ("serviceWorker" in navigator) {
+    console.log('Service worker registered');
+    navigator.serviceWorker.register("/sw.js");
+}
